45f9160796b08dfa5a99e686080277796cc91b3d
Yacine Djenidi <djenidi.dev@gmail.com>
2024-01-06 01:00:49 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(process.env.SALAH);
 });
 
 app.listen(process.env.THPORT, () => {
